13137a1d39af500b4a85d68420feaca5ad3876a4
Ryan <ryankanno@localkinegrinds.com>
2016-05-27 13:27:20 -0400

    Updated front page according to B's changes

--- uipa_org/theme/urls.py
+++ uipa_org/theme/urls.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 Ryan Kanno <ryankanno@localkinegrinds.com>
+#
+# Distributed under terms of the MIT license.
+
 from django.conf.urls import patterns
+from django.conf.urls import url
+from django.contrib.flatpages.views import flatpage
+from django.utils.translation import ugettext as _
 
+faq_url_part = _('faq')
 
-urlpatterns = patterns('theme.views',
-    # put your custom urls here
+urlpatterns = patterns('',
+    url(r'^%s/$' % faq_url_part, flatpage,
+        {'url': '/%s/' % faq_url_part}, name='faq-index'),
 )
+
+# vim: fenc=utf-8
+# vim: filetype=python
