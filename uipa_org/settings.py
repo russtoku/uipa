afeb2c9245f6a12a1184edbfa2978194843ae668
Ryan <ryankanno@localkinegrinds.com>
2016-05-28 04:15:28 -0400

    Removed whoosh. Added solr

--- uipa_org/settings.py
+++ uipa_org/settings.py
@@ -57,9 +57,9 @@ class UipaOrgThemeBase(ThemeBase):
 
     HAYSTACK_CONNECTIONS = {
         'default': {
-            'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-            'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-        },
+            'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+            'URL': 'http://127.0.0.1:8983/solr/'
+        }
     }
 
     @property
