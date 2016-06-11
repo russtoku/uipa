de00f5f0761d54e7627712480326c42d03f1c155
Ryan <ryankanno@localkinegrinds.com>
2016-06-11 13:22:43 -0400

    Fixed up data. Added new reset command

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -f './dev.db' 2>/dev/null && python manage.py migrate && python manage.py createsuperuser --username uipa --email admin@uipa.org && python manage.py loaddata uipa_org/fixtures/* && python manage.py import_csv 'data/2016-05-27-Hawaii_UIPA_Public_Bodies_All.csv'
+rm -f './dev.db' 2>/dev/null && python manage.py migrate && python manage.py createsuperuser --username uipa --email admin@uipa.org && python manage.py loaddata uipa_org/fixtures/* && python manage.py import_csv 'data/2016-06-11-Hawaii_UIPA_Public_Bodies_All.csv'
