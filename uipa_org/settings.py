99e2af79be09b7342510ab4c22288fdb70c02376
Ryan <ryankanno@localkinegrinds.com>
2016-07-26 21:58:55 -0400

    Added ElasticSearch to beta configuration (installed on Beta machine)

--- uipa_org/settings.py
+++ uipa_org/settings.py
@@ -121,8 +121,9 @@ class Beta(UipaOrgThemeBase, Base):
 
     HAYSTACK_CONNECTIONS = {
         'default': {
-            'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-            'URL': 'http://127.0.0.1:8983/solr/'
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': 'http://127.0.0.1:9200/',
+            'INDEX_NAME': 'haystack',
         }
     }
 
