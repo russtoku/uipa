0b2ebefc80105a7944dca215135ba6d35da522e9
Ryan <ryankanno@localkinegrinds.com>
2016-09-14 19:48:40 -1000

    Added fake SECRET_KEY

--- uipa_org/settings.py
+++ uipa_org/settings.py
@@ -21,6 +21,8 @@ class UipaOrgThemeBase(ThemeBase):
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, "..", "public"))
 
+    SECRET_KEY = 'asdfasdfasdfafdakasdfadjfaskfd'
+
     FIXTURE_DIRS = ('fixtures',)
 
     @property
