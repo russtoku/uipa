ddc1b3463c8bdd62dd8557652ef27659c86df778
Ryan <ryankanno@localkinegrinds.com>
2016-09-14 20:06:26 -1000

    Renamed to celery_uipa.py

--- uipa_org/celery_uipa.py
+++ uipa_org/celery_uipa.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-import os
-
 # This should have been added to supervisord
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uipa_org.settings')
 # os.environ.setdefault("DJANGO_CONFIGURATION", "Beta")
@@ -15,6 +13,6 @@ from django.conf import settings
 import configurations
 configurations.setup()
 
-app = Celery('froide')
+app = Celery('uipa_org')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, related_name='tasks')
