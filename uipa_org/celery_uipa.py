a06313b64c530589b76268d2b2bd86eabd5b870e
Ryan <ryankanno@localkinegrinds.com>
2016-09-15 20:27:54 -1000

    One more attempt at using celery_uipa as the celery app and import

---
+++ uipa_org/celery_uipa.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# This should have been added to supervisord
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uipa_org.settings')
+# os.environ.setdefault("DJANGO_CONFIGURATION", "Beta")
+
+from celery import Celery
+from django.conf import settings
+
+import configurations
+configurations.setup()
+
+app = Celery('uipa_org')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, related_name='tasks')
