96fbb3b9ea09a6041077aff63bc5c44b50df3e55
Ryan <ryankanno@localkinegrinds.com>
2016-10-05 21:11:20 -1000

    Added in logic to detect_private_will_be_made_public and make_private_public

--- uipa_org/tasks.py
+++ uipa_org/tasks.py
@@ -5,9 +5,54 @@
 #
 # Distributed under terms of the MIT license.
 
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from django.core.mail import send_mail
+from django.db.models import Q
+from django.template.loader import render_to_string
+from django.utils import translation
+from froide.models import FoiRequest
 from uipa_org.celery import app as celery_app
+import timedelta
+
+
+logger = get_task_logger(__name__)
+
+NOTIFICATION_SENT_BEFORE_NUM_DAYS = 14
+
 
 @celery_app.task
-def test(*args, **kwargs):
-    logger = test.get_logger()
-    logger.info("TESTING")
+def detect_private_will_be_made_public(*args, **kwargs):
+    translation.activate(settings.LANGUAGE_CODE)
+    num_days_after_due_date = settings.FROIDE_CONFIG.get(
+        'make_public_num_days_after_due_date', 365)
+    num_days_after_due_date = num_days_after_due_date - NOTIFICATION_SENT_BEFORE_NUM_DAYS
+
+    now = timezone.now()
+    due_date_everything_should_be_made_private = now - timedelta(num_days_after_due_date)
+
+    for foirequest in FoiRequest.objects.filter(Q(visibility=0) | Q(visibility=1), is_foi=True, due_date=due_date_everything_should_be_made_private):
+        send_mail(u'{0}'.format(
+                _("%(site_name)s: Reminder that your request is being made public in 7 days") % {
+                    "site_name": settings.SITE_NAME
+                },
+            ),
+            render_to_string("foirequest/emails/became_public.txt", {
+                "site_name": settings.SITE_NAME,
+                "request": foirequest,
+            }),
+            settings.DEFAULT_FROM_EMAIL,
+            [foirequest.user.email]
+        )
+
+
+@celery_app.task
+def make_private_public(*args, **kwargs):
+    translation.activate(settings.LANGUAGE_CODE)
+    num_days_after_due_date = settings.FROIDE_CONFIG.get(
+        'make_public_num_days_after_due_date', 365)
+    now = timezone.now()
+    due_date_everything_should_be_made_private = now - timedelta(num_days_after_due_date)
+    for foirequest in FoiRequest.objects.filter(Q(visibility=0) | Q(visibility=1), is_foi=True, due_date__lte=due_date_everything_should_be_made_private):
+        foirequest.visibility = 2
+        foirequest.save()
