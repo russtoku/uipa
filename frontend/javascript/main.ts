24caaad0322cb03f3810036f46c6d8b695fa93f7
Tyler Chong <tyliec@github.com>
2024-03-30 00:15:14 -1000

    fix: navbar dropdown (#86)

--- frontend/javascript/main.ts
+++ frontend/javascript/main.ts
@@ -1,1 +1,9 @@
 import '../styles/main.scss'
+
+import 'froide/frontend/javascript/snippets/bootstrap'
+import 'froide/frontend/javascript/snippets/copy-input'
+import 'froide/frontend/javascript/snippets/form-ajax'
+import 'froide/frontend/javascript/snippets/misc'
+import 'froide/frontend/javascript/snippets/search'
+import 'froide/frontend/javascript/snippets/inline-edit-forms'
+import 'froide/frontend/javascript/snippets/color-mode'
