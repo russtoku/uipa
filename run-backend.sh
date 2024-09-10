3ae1269fad155c21357e47886dbcb73dc6e514c0
Suchandra Thapa <sthapa@ci.uchicago.edu>
2024-09-09 19:26:12 -1000

    Add code to gate features in the containers

--- run-backend.sh
+++ run-backend.sh
@@ -1,19 +1,23 @@
 #!/bin/ash
 
-
-#yarn link
 yarn install
 yarn build
 yarn serve dev &
 
-# hack until we get the froide setup build working (i.e. include templates)
-echo "STARTING DB"
 # To initialise the database:
 /root/.local/bin/poetry run ./manage.py migrate --skip-checks
-# Create a superuser
-/root/.local/bin/poetry run ./manage.py createsuperuser
-# Create and populate search index
-/root/.local/bin/poetry run ./manage.py search_index --create
-/root/.local/bin/poetry run ./manage.py search_index --populate
+
+if [[ -z "${INITIALIZE_DB}" ]];
+then
+  # Create and populate search index
+  /root/.local/bin/poetry run ./manage.py search_index --create
+  /root/.local/bin/poetry run ./manage.py search_index --populate
+fi
+
+if [[ -z "${LOAD_DATA}" ]];
+then
+  echo "Load the seed data..."
+  /root/.local/bin/poetry run ./manage.py loaddata uipa_org/fixtures/*
+fi
 
 /root/.local/bin/poetry run /bin/ash /srv/django/run-django.sh
